e333ba8ac5b465e22113592ad00cb302785c3946
RAGS <rmramesh@IN-Rmramesh.local>
2019-09-22 00:26:49 +0530

    Add undertow

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ * @author Stuart Douglas
+ */
+public class Application {
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
